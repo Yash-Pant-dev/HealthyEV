f26b740d1e13005f02d423a5c20906fed996263f
Yash-Pant-dev <yashpantdev@gmail.com>
2022-01-23 03:06:26 +0530

    Add different tabs and required input button. Create rudimentery design of UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var batteryName = 'Nexon';
+  var temp = '-';
+  var dischargeCycles = '-';
+  var capacity = 46;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
-            title: const Text('Fixed Tabs'),
+            leading: const Icon(Icons.car_repair),
+            title: const Text(
+              'Healthy EV',
+              style: TextStyle(
+                fontFamily: 'Poppins', //FIXME: Poppins doesnt apply
+                fontSize: 25,
+              ),
+            ),
             automaticallyImplyLeading: false,
-            backgroundColor: const Color(0xff5808e5),
-            bottom: const TabBar(
+            backgroundColor: Colors.blue,
+            bottom: TabBar(
               indicatorColor: Colors.white,
               tabs: [
-                Tab(text: 'DOGS', icon: Icon(Icons.favorite)),
-                Tab(text: 'CATS', icon: Icon(Icons.music_note)),
-                Tab(text: 'BIRDS', icon: Icon(Icons.search)),
+                Tab(
+                  child: Center(
+                    child: Row(
+                      children: const [
+                        Icon(Icons.analytics_outlined),
+                        Text('ANALYSIS')
+                      ],
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    children: const [
+                      Icon(Icons.monitor_rounded),
+                      Text('DATA AND INPUT')
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-          body: const TabBarView(
+          body: TabBarView(
             children: [
-              Center(child: Text('DOGS')),
-              Center(child: Text('CATS')),
-              Center(child: Text('BIRDS')),
+              Card(
+                clipBehavior: Clip.antiAlias,
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: Image.asset('assets/carbattery.png', ),
+                      title: const Text('At a Glance'),
+                      subtitle: Text(
+                        'Battery Details: $batteryName $capacity' + 'Whr',
+                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      ),
+                    ),
+                    Image.asset('assets/batterywear.jpg',),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Average Ambient Temp.: $temp   Discharge Cycles: $dischargeCycles' +
+                            'WHr',
+                        style: TextStyle(color: Colors.black.withOpacity(0.9)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Scaffold(
+                body: Container(
+                  child: const Text('hi'),
+                ),
+                floatingActionButton: FloatingActionButton.extended(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                  label: const Text('Add New Data'),
+                  backgroundColor: Colors.blue,
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-}
+} //TODO: Add toast/snackbar when data updates
